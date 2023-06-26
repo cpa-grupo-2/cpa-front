da3613349d429062c78a9f00f865d658357c734e
luizsaling <luizsaling@gmail.com>
2023-06-26 00:40:14 -0300

    FEAT: criado serviço para tela de eixos, funções cadastrar e listar.

--- src/services/EixosService.js
+++ src/services/EixosService.js
@@ -1,15 +1,43 @@
 import api from './api.js';
 import { Component } from 'react';
 import messageService from '../services/MessageService.js'
+
 export class EixosService extends Component {
     async cadastroEixo(nomeEixo, descricao) {
         const params = {
-            eixo: nomeEixo,
+            nomeEixo: nomeEixo,
             descricao: descricao
+        }
+        console.log({ params })
+        return await api.post(`/api/eixo`, params)
+            .then((response) => {
+                messageService.successMessage('Eixo cadastrado com sucesso')
+                console.log({ response })
+                const data = {
+                    'token': response.data.token,
+                    'role': response.data.level,
+                }
+                console.log({ data })
+                return data;
+            })
+            .catch((err) => {
+                messageService.errorMessage('Erro ao cadastrar Eixo')
+                console.log("Ops! ocorreu um erro" + err);
+            });
+    }
+
+
+    async listarEixos() {
+        try {
+            const response = await api.get('api/eixo/eixos')
+            return response.data;
+        } catch (err) {
+            messageService.errorMessage('Não há eixos cadastrados')
+            console.log("Ops! ocorreu um erro" + err);
         }
     }
 
 }
 
-const cadastroService = new EixosService();
-export default cadastroService;
+const eixosService = new EixosService();
+export default eixosService;
